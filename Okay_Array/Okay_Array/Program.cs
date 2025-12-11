9a9cb7f175751219e30e22ee05114d6031a54e13
ACT-STUDENT <ACT-STUDENT@LAB2-PC43>
2025-12-11 16:58:50 +0800

    okay

---
+++ Okay_Array/Okay_Array/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okay_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            ////    int[] numbers = { 1, 3, 5, 6, 2 };
+            ////    Array.Reverse(numbers);
+
+            ////    foreach (int num in numbers)
+            ////    {
+            ////        Console.WriteLine(num);
+            ////    }
+
+
+
+            ////    for (int num = 0; num <= 10; ++num)
+            ////    {
+            ////        Console.WriteLine(num);
+            ////    }
+            ////    Console.ReadKey();
+            ///
+
+
+
+            Console.Clear();
+            Console.Write("Select number (1/2) : \t");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 1)
+            {
+                Console.WriteLine("2V2");
+                Console.WriteLine("Select Your Fighter Barili Area\t");
+                string Fname = Console.ReadLine();
+
+                if (Fname == "Kons")
+                {
+
+                }
+
+                string[] name = {"ATILLO FROM BARILI !!","Jayvin Kababat\n"};
+                Array.Sort(name);
+
+                foreach (string Name in name)
+                {
+                    Console.WriteLine(Name);
+                }
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("2v2");
+
+                Console.WriteLine("Select Your Fighter Bohol Area\t");
+                string Tname = Console.ReadLine();
+
+                 if(Tname == "Carls")
+                {
+
+                }
+                string[] Sname = { "Carls Taga Bohol","Bedro Kinamot\n"};
+                Array.Sort(Sname);
+                foreach (string Number in Sname)
+                {
+                    Console.WriteLine(Number);
+                }
+            }
+            char choice;
+
+            Console.WriteLine("\n\nDo you want to go back? (y/n):\n\n\t");
+            choice = Convert.ToChar(Console.ReadLine());
+
+            if (choice == 'y')
+            {
+                goto Main;
+            }
+            else if (choice == 'n')
+            {
+                Console.WriteLine("bYE!!!");
+                return;
+                //}
+                //while (true)
+                //{
+                //    Console.WriteLine("UserName:\t");
+                //    Name = Console.ReadLine();
+
+                //    Console.WriteLine("Password:\t");
+                //    Pass = Convert.ToInt32(Console.ReadLine());
+
+                //    if (Name == "atillo")
+                //    {
+                //        Console.WriteLine("Login Succesfully");
+                //        break;
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("Try Again!");
+                //    }
+
+                //    Console.ReadKey();
+            }
+       }
+    }
+}
---
+++ Okay_Array/Okay_Array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Okay_Array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Okay_Array")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa8aef7f-8c06-4a67-ad7e-56b316b57001")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
