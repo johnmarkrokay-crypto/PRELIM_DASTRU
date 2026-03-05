47ec0e69b2ed6f300ad715779492ccd74088c030
John Mark Okay <johnmarkrokay@gmail.com>
2026-03-05 16:53:26 +0800

    ACT10
    act10

---
+++ ACTIVITY 10 Queue in OOP Approach/MyQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTIVITY_10_Queue_in_OOP_Approach
+{
+    internal class MyQueue
+    {
+        List<string> queue = new List<string>(); // empty queue
+
+        public void Enqueue(string item)
+        {
+            queue.Add(item);
+            Console.WriteLine("\tAdded: " + item);
+        }
+
+        public void Dequeue()
+        {
+            try
+            {
+                string item = queue[0];
+                queue.RemoveAt(0);
+                Console.WriteLine("\tRemoved: " + item);
+            }
+            catch
+            {
+                Console.WriteLine("Queue is empty");
+            }
+        }
+
+        public void Display()
+        {
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Console.WriteLine("Queue: ");
+                foreach (string item in queue)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
+
---
+++ ACTIVITY 10 Queue in OOP Approach/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ACTIVITY_10_Queue_in_OOP_Approach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue myQueue = new MyQueue();
+
+            //Name: John Mark R. Okay                 Section: IT401P
+            //ACTIVITY 10: Queue in OOP Approach
+
+            while (true)
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("Here's are the choices!");
+                Console.WriteLine("1. Enque:\t");
+                Console.WriteLine("2. Deque:\t");
+                Console.WriteLine("3.Display:\t");
+                Console.WriteLine("4.Exit:\t");
+                Console.WriteLine(Environment.NewLine);
+
+                try
+                {
+                    Console.Write("\tEnter a choices:\t");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (choice == 1)
+                    {
+                        Console.Write("\nEnter a Item to Enque:\t");
+                        string items = Console.ReadLine();
+                        myQueue.Enqueue(items);
+                    }
+                    else if (choice == 2)
+                    {
+                        myQueue.Dequeue();
+
+                    }
+                    else if (choice == 3)
+                    {
+                        myQueue.Display();
+                    }
+
+                    else if (choice == 4)
+                    {
+                        Console.WriteLine("\tBye!!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                    }
+                }
+                catch
+                {
+                     Console.WriteLine("Enter Number Only for Choices");
+                }
+            }
+        }
+    }
+}
+
+  
+
+        
+    
+
